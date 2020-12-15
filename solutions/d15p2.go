413bec3b58a635a9199effe6791e2713f3bc4082
Serdar Kabaoglu <serdarka@gmail.com>
2020-12-15 14:30:46 +0100

    day 15 speed optimised

--- solutions/d15p2.go
+++ solutions/d15p2.go
@@ -8,24 +8,34 @@ import (
 
 func D15P2(input []string) string {
 	strNums := strings.Split(input[0], ",")
-	nums := []int{}
+	nums := []int32{}
 	for _, s := range strNums {
 		n, _ := strconv.Atoi(s)
-		nums = append(nums, n)
+		nums = append(nums, int32(n))
 	}
 
-	turn := 0
-	mem := map[int]int{}
-	num := 0
+	var turn int32 = 0
+	var num int32 = 0
+	var lomemSize int32 = 10000000
+	mem := map[int32]int32{}
+	lomem := make([]int32, lomemSize)
 
-	for ; turn < len(nums); turn++ {
+	for ; turn < int32(len(nums)); turn++ {
 		num = nums[turn]
-		mem[num] = turn + 1
+		lomem[num] = turn + 1
 	}
 
 	for ; turn != 30000000; turn++ {
-		m, ok := mem[num]
-		mem[num] = turn
+		var m int32
+		var ok bool
+		if num < lomemSize {
+			m = lomem[num]
+			ok = m != 0
+			lomem[num] = turn
+		} else {
+			m, ok = mem[num]
+			mem[num] = turn
+		}
 
 		if !ok {
 			num = 0
